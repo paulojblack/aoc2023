a0501ec9b3440c5ad2e25c5790d493404e95b38c
Paulo Black <paulo@pauloblack.com>
2024-09-08 18:40:04 -0400

    working solve for 2a

--- 2.go
+++ 2.go
@@ -1,6 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type cubeConfig struct {
+	Color string
+}
+
+var totalCubes = map[string]int{
+	"red":   12,
+	"green": 13,
+	"blue":  14,
+}
 
 func two() error {
 	lc := make(chan string)
@@ -11,9 +25,32 @@ func two() error {
 		}
 	}()
 
+	totalSum := 0
 	for line := range lc {
-		fmt.Println(line)
-	}
+		countMeIn := true
+		parts := strings.Split(line, ":")
 
+		id, _ := strconv.Atoi(strings.Split(parts[0], " ")[1])
+		rolls := strings.Split(parts[1], ";")
+
+		for _, roll := range rolls {
+			cubeRolls := strings.Split(roll, ",")
+
+			for _, cubeCount := range cubeRolls {
+				cubeParts := strings.Split(strings.TrimLeft(cubeCount, " "), " ")
+				intCubeRollCount, _ := strconv.Atoi(cubeParts[0])
+
+				if totalCubes[cubeParts[1]] < intCubeRollCount {
+					countMeIn = false
+				}
+			}
+		}
+
+		if countMeIn == true {
+			totalSum += id
+		}
+
+	}
+	fmt.Println(totalSum)
 	return nil
 }
